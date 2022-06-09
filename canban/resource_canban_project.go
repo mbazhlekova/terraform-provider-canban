9cf11ea95088cfadedc8b5288762b2988cb3793a
Maria Bazhlekova <mbazhlek@gmail.com>
2022-06-09 07:57:41 -0500

    chore: add resource file

---
+++ canban/resource_canban_project.go
@@ -0,0 +1,43 @@
+package canban
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceProject() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceProjectCreate,
+		UpdateContext: resourceProjectUpdate,
+		ReadContext:   resourceProjectRead,
+		DeleteContext: resourceProjectDelete,
+		Schema:        map[string]*schema.Schema{},
+	}
+}
+
+func resourceProjectCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	return diags
+}
+
+func resourceProjectRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	return diags
+}
+
+func resourceProjectUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return resourceProjectRead(ctx, d, m)
+}
+
+func resourceProjectDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	return diags
+}
